057c1085a1f8f87f1e34edfd2e6e859c9582db14
Zeren Yan <zyan30@sundevils.asu.edu>
2018-10-27 20:36:56 -0700

    migrate the model

--- userReg/admin.py
+++ userReg/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from userReg.models import User,Content,Comment
 # Register your models here.
+admin.site.register([User,Content,Comment])
--- userReg/models.py
+++ userReg/models.py
@@ -1,15 +1,28 @@
 from django.db import models
-import datetime
+import django.utils.timezone as timezone
 # Create your models here.
 
-Class User(models.Model):
+class User(models.Model):
 		uid = models.IntegerField(primary_key=True)
-		uname=models.CharField(max_lenth=50)
-		upassword=models.CharField(max_lenth=16)
-		gender=models.CharField(max_lenth=6)
-        regTime=models.DateField(_("Date"),default=datetime.date.today)
-        email=models.CharField(max_length=50)
+		uname=models.CharField(max_length=50)
+		upassword=models.CharField(max_length=16)
+		gender=(
+				(0,"male"),
+				(1,"female"),
+				(2,"unknown")
+			)
+		regTime=models.DateField('regDate',auto_now=True)
+		email=models.EmailField(max_length=50)
+		img= models.ImageField(upload_to='upload')
 
 
-Class Content(models.Model):
-		cid=models
+class Content(models.Model):
+		cid=models.IntegerField(primary_key=True)
+		uid=models.ForeignKey('User',on_delete=models.CASCADE)
+		contentword=models.CharField(max_length=300)
+
+class Comment(models.Model):
+		commentID=models.IntegerField(primary_key=True)
+		cid=models.ForeignKey('Content',on_delete=models.CASCADE)
+		commentword=models.CharField(max_length=150)
+		uid=models.ForeignKey('User',on_delete=models.CASCADE)
